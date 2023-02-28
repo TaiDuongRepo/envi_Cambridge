f66c0b948750ae9a8f36fe23260cfbac7cf01cee
TaiDuongRepo <taiduong1301@gmail.com>
2023-02-28 22:10:59 +0700

    vietnamese

--- envi_Cambridge.js
+++ envi_Cambridge.js
@@ -1,5 +1,5 @@
 /* global api */
-class envi_Cambridge {
+class enen_Cambridge {
     constructor(options) {
         this.options = options;
         this.maxexample = 2;
@@ -45,34 +45,26 @@ class envi_Cambridge {
             return [];
         }
 
-        let entries = doc.querySelectorAll('.pr .entry-body__el') || [];
+        let entries = doc.querySelectorAll('.pr .entry-body .d') || [];
         for (const entry of entries) {
             let definitions = [];
             let audios = [];
 
-            let expression = T(entry.querySelector('.headword'));
+            let expression = T(entry.querySelector('.tw-bw'));
             let reading = '';
             let readings = entry.querySelectorAll('.pron .ipa');
-            if (readings) {
-                let reading_uk = T(readings[0]);
-                let reading_us = T(readings[1]);
-                reading = (reading_uk || reading_us) ? `UK[${reading_uk}] US[${reading_us}] ` : '';
-            }
-            let pos = T(entry.querySelector('.posgram'));
+            // if (readings) {
+            //     let reading_uk = T(readings[0]);
+            //     let reading_us = T(readings[1]);
+            //     reading = (reading_uk || reading_us) ? `UK[${reading_uk}] US[${reading_us}] ` : '';
+            // }
+            let pos = T(entry.querySelector('.dpos-g'));
             pos = pos ? `<span class='pos'>${pos}</span>` : '';
             audios[0] = entry.querySelector(".uk.dpron-i source");
-            audios[0] = audios[0] ? 'https://dictionary.cambridge.org' + audios[0].getAttribute('src') : null;
-            if (audios[0] === null) {
-                audios[0] = entry.querySelector(".uk.dloc source");
-                audios[0] = audios[0] ? 'https://dictionary.cambridge.org' + audios[0].getAttribute('src') : '';
-            }
+            audios[0] = audios[0] ? 'https://dictionary.cambridge.org' + audios[0].getAttribute('src') : '';
             //audios[0] = audios[0].replace('https', 'http');
             audios[1] = entry.querySelector(".us.dpron-i source");
             audios[1] = audios[1] ? 'https://dictionary.cambridge.org' + audios[1].getAttribute('src') : '';
-            if (audios[1] === null) {
-                audios[1] = entry.querySelector(".us.dloc source");
-                audios[1] = audios[1] ? 'https://dictionary.cambridge.org' + audios[1].getAttribute('src') : '';
-            }
             //audios[1] = audios[1].replace('https', 'http');
 
             let sensbodys = entry.querySelectorAll('.sense-body') || [];
@@ -94,12 +86,12 @@ class envi_Cambridge {
                     // make definition segement
                     for (const defblock of defblocks) {
                         let eng_tran = T(defblock.querySelector('.ddef_h .def'));
-                        let vi_tran = T(defblock.querySelector('.def-body .trans'));
+                        let chn_tran = T(defblock.querySelector('.def-body .trans'));
                         if (!eng_tran) continue;
                         let definition = '';
                         eng_tran = `<span class='eng_tran'>${eng_tran.replace(RegExp(expression, 'gi'),`<b>${expression}</b>`)}</span>`;
-                        vi_tran = `<br /><span class='vi_tran'>${vi_tran}</span>`;
-                        let tran = `<span class='tran'>${eng_tran}${vi_tran}</span>`;
+                        chn_tran = `<span class='chn_tran'>${chn_tran}</span>`;
+                        let tran = `<span class='tran'>${eng_tran}${chn_tran}</span>`;
                         definition += phrasehead ? `${phrasehead}${tran}` : `${pos}${tran}`;
 
                         // make exmaple segement
@@ -109,8 +101,8 @@ class envi_Cambridge {
                             for (const [index, examp] of examps.entries()) {
                                 if (index > this.maxexample - 1) break; // to control only 2 example sentence.
                                 let eng_examp = T(examp.querySelector('.eg'));
-                                let vi_examp = T(examp.querySelector('.trans'));
-                                definition += `<li class='sent'><span class='eng_sent'>${eng_examp.replace(RegExp(expression, 'gi'),`<b>${expression}</b>`)}</span><span class='vi_sent'>${vi_examp}</span></li>`;
+                                let chn_examp = T(examp.querySelector('.trans'));
+                                definition += `<li class='sent'><span class='eng_sent'>${eng_examp.replace(RegExp(expression, 'gi'),`<b>${expression}</b>`)}</span><span class='chn_sent'>${chn_examp}</span></li>`;
                             }
                             definition += '</ul>';
                         }
@@ -138,11 +130,11 @@ class envi_Cambridge {
                 span.pos  {text-transform:lowercase; font-size:0.9em; margin-right:5px; padding:2px 4px; color:white; background-color:#0d47a1; border-radius:3px;}
                 span.tran {margin:0; padding:0;}
                 span.eng_tran {margin-right:3px; padding:0;}
-                span.vi_tran {color:#0d47a1;}
+                span.chn_tran {color:#0d47a1;}
                 ul.sents {font-size:0.8em; list-style:square inside; margin:3px 0;padding:5px;background:rgba(13,71,161,0.1); border-radius:5px;}
                 li.sent  {margin:0; padding:0;}
                 span.eng_sent {margin-right:5px;}
-                span.vi_sent {color:#0d47a1;}
+                span.chn_sent {color:#0d47a1;}
             </style>`;
         return css;
     }
